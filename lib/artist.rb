5ace8a2a7ebc50846df61107850b14661e31072e
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2021-01-10 21:43:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,12 +19,6 @@ class Artist
     Song.all.select {|song| song.artist == self}
   end
 
-  def self.create(name)
-    artist = self.new(name)
-    artist.name = name
-    artist
-  end
-
   def self.find(name)
     self.all.detect { |artist| artist.name == name }
   end
